0d06b38b6dc83b94a8711d59e389ebd992f1c97a
MarcyIO <bookstore707@gmail.com>
2019-10-16 12:48:26 -0400

    refactored code structure

--- src/index.js
+++ src/index.js
@@ -1,12 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import SeasonDisplay from "./components/seasonDisplay/SeasonDisplay";
+import Loader from "./components/loader/Loader"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
+  // this short hand is the equivalent to a constructor in babel
+  state = {
+    latitude: null,
+    errorMessage: ""
+  };
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  componentDidMount() {
+    console.log("My component mounted");
+    let getLocation = position => {
+      this.setState({ latitude: position.coords.latitude });
+      console.log(this.state.latitude);
+    };
+
+    let errorLocation = err => {
+      console.log(err);
+      this.setState({ errorMessage: err.message });
+    };
+
+    window.navigator.geolocation.getCurrentPosition(getLocation, errorLocation);
+  }
+
+  renderContent(){
+      // this pattern allows me not to have multiple return statement inside a render method
+      // when ever I have conditional logic put it in a helper method
+      if (this.state.errorMessage && !this.state.latitude) {
+        return <div> Error: {this.state.errorMessage}</div>;
+      } else if (!this.state.errorMessage && !this.state.latitude) {
+        return <Loader message="Please allow location access..." />;
+      } else {
+        return <SeasonDisplay lat={this.state.latitude} />;
+      }
+  }
+  render() {
+   return (
+     <div className="app">
+       {this.renderContent()}
+     </div>
+   )
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
